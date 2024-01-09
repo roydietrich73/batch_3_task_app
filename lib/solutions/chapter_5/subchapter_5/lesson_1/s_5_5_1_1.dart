7c79bb7d6225e68cb7e8b8214398aa2fc8ac00d1
roydietrich73 <150008930+roydietrich73@users.noreply.github.com>
2024-01-09 15:27:26 +0100

    5511

--- lib/solutions/chapter_5/subchapter_5/lesson_1/s_5_5_1_1.dart
+++ lib/solutions/chapter_5/subchapter_5/lesson_1/s_5_5_1_1.dart
@@ -4,12 +4,46 @@ class S5511 extends StatelessWidget {
   const S5511({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return AsyncMethods();
   }
 
   // ignoriere für den Moment den Inhalt dieser Methode
   // nutze "await warteKurz();" in deinem Code, um einen Moment zu warten
   Future<void> warteKurz() {
     return Future.delayed(const Duration(milliseconds: 500));
+  }
+}
+class AsyncMethods extends StatelessWidget {
+ Future<void> ersteAsyncMethode() async{
+  await Future.delayed(Duration(seconds: 2));
+  print('Erste asyncrone Methode');
+ }
+ Future<void> zweiteAsyncMethode() async {
+  await Future.delayed(Duration(seconds: 3));
+  print('Zweite asynchrone Methode');
+ }
+ Future<void> beideAsyncMethoden() async {
+  await ersteAsyncMethode();
+  await zweiteAsyncMethode();
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: const Text('ElevatedButton'),
+    ),
+    body: Center(
+      child: ElevatedButton(
+        onPressed:()async{
+          await beideAsyncMethoden();
+            print('Fertig');
+          },
+        
+        child: const Text('Async Methoden ausführen'),
+        ),
+        ),
+    );
+    
   }
 }
