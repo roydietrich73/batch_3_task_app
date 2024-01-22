f8f6f3e4535f5c45896e4ba667ffe93d69b7c74e
roydietrich73 <150008930+roydietrich73@users.noreply.github.com>
2024-01-22 11:57:51 +0100

    batch_3_task

--- lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
+++ lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
@@ -7,3 +7,35 @@ class S5411 extends StatelessWidget {
     throw UnimplementedError();
   }
 }
+void main(){
+  runApp(
+  MaterialApp(
+    home:MyCatchPage(),
+    ),
+    );
+}
+class MyCatchPage extends StatelessWidget {
+  late String nullString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title: const Text('Try Catch'),
+    ),
+    body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+        try{
+          print(nullString.length);
+        }catch (error) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Fehler: $error'),
+          ));
+        }
+        },
+        child: const Text('Klicken für einen Fehler!'),
+        ),
+      ),
+    );
+  }
+}
